f01707df38b7bb8db8e43bc1e744f5fc454e6b5c
Wes Todd <wes@wesleytodd.com>
2016-02-27 13:30:05 -0600

    added streaming methods

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 var path = require('path');
 var parallel = require('run-parallel');
+var through2 = require('through2');
 
 module.exports = foreachInDir;
 module.exports.recursive = recursiveForeachInDir;
+module.exports.stream = streamForEachInDir;
+module.exports.streamRecursive = streamRecursiveForEachInDir;
 
 // Execute a callback for each file in a directory
 function foreachInDir (dirpath, fnc, done) {
@@ -32,11 +35,40 @@ function recursiveForeachInDir (dirpath, fnc, done, __origDirPath) {
 			fnc(filepath.replace((__origDirPath || dirpath) + '/', ''), function () {
 				// If directory, recurse into it
 				if (stat.isDirectory()) {
-					return recursiveForeachInDir(filepath, fnc, done2, __origDirPath || dirpath, done2);
+					return recursiveForeachInDir(filepath, fnc, done2, __origDirPath || dirpath);
 				}
 
 				done2();
 			});
 		});
 	}, done);
+}
+
+function streamForEachInDir (dirpath) {
+	var stream = through2.obj();
+	forEachInDir(dirpath, function (file, done) {
+		stream.write(file);
+		done();
+	}, function (err) {
+		if (err) {
+			return stream.emit('error', err);
+		}
+		stream.end();
+	});
+	return stream;
+}
+
+function streamRecursiveForEachInDir (dirpath) {
+	var stream = through2();
+	stream.setEncoding('utf8');
+	recursiveForeachInDir(dirpath, function (file, done) {
+		stream.write(file, 'utf8');
+		done();
+	}, function (err) {
+		if (err) {
+			return stream.emit('error', err);
+		}
+		stream.end();
+	});
+	return stream;
 }
